8553cf2ce7514745195e57338cc9e28a3053d6dd
Napat <napat.wongchr@gmail.com>
2022-03-08 22:19:49 +0700

    add answer on ex-7

--- ex-7.js
+++ ex-7.js
@@ -1,5 +1,16 @@
 // Start coding here !
 
+function getProduct(name = "John", id = "001") {
+  let code = `${name}-${id}`;
+  return {
+    id: 1030,
+    name: "Samsung",
+    price: 3000,
+    photo: null,
+    code: code,
+  };
+}
+
 let product = getProduct();
 console.log(product);
 
